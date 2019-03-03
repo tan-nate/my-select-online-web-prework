0d158d54d66c67a32ee37759c972acef21376d7d
Nate Tan <nktan93@gmail.com>
2019-03-03 23:42:08 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
-def my_select(collection)
+require 'pry'
 
+def my_select(array)
+  i = 0
+  new_array = []
+  while i < array.length
+    new_array << yield(array[i])
+    i = i + 1
+  end
+  
+  new_array
 end
+
+my_collect(array) do |name|
+  name.capitalize
+end
+
