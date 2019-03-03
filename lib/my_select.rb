87af5ed8b24f849ea8c46df40e1ee5e65e793280
Nate Tan <nktan93@gmail.com>
2019-03-03 23:49:55 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -7,7 +7,7 @@ def my_select(array)
   new_array = []
   while i < array.length
     if yield(array[i]) === true
-      new_array << yield(array[i])
+      new_array << array[i]
     end
     i = i + 1
   end
